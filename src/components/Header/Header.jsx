3ea844a513be32dda5755b4e95eb8fcadb36ad45
Mohamed Hussein <mohamedmhussein@protonmail.com>
2023-12-25 18:53:19 -0500

    Simplifies the header jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,13 +4,30 @@ import styles from './Header.module.css'
 
 
 const Header = () => {
+
+  const items = [
+    {
+      title: "Home",
+      link: "/"
+    },
+    {
+      title: "Recipes",
+      link: "/recipes"
+    },
+    {
+      title: "Categories",
+      link: "/categories"
+    }
+  ]
+
   return (
     <nav className={styles.container}>
         <Link href="/" className= {styles.logo}>Recipe Exchange</Link>
         <div className={styles.links}>
-            <div>Home</div>
-            <div>Recipes</div>
-            <div>Categories</div>
+          {items.map(item => 
+          (<Link className = {styles.link}href={item.link} key = {item.title} >{item.title}</Link>)
+          )}
+
             {/* {More navigation items} */}
         </div>
 
