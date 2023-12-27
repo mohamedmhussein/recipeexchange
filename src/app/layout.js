9ef4925c321aba921203481de3c6039dd7fd20ce
Mohamed Hussein <mohamedmhussein@protonmail.com>
2023-12-27 18:50:38 -0500

    Styles the header links

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,9 @@
-import { Inter } from 'next/font/google'
+import { Petrona } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/Header/Header'
 import Footer from '@/components/Footer/Footer'
 import Image from 'next/image'
-const inter = Inter({ subsets: ['latin'] })
+const petrona = Petrona({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={petrona.className}>
         <div className='container'>
           <Header />
           {children}
