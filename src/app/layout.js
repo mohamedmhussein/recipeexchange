4790eafa154d7e0c7433ced175826f5b2718b40f
Mohamed Hussein <mohamedmhussein@protonmail.com>
2023-12-30 11:08:54 -0500

    Fixes the main background

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,6 +19,7 @@ export default function RootLayout({ children }) {
           {children}
           <Footer />
         </div>
+        <div className='background-container'></div>
       </body>
     </html>
   )
